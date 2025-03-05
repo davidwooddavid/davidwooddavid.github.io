841d9f0693c138e65e8e6177db558a6d357b0668
davidwooddavid <43978638+davidwooddavid@users.noreply.github.com>
2025-03-06 07:53:51 +1100

    added menu

---
+++ menu.js
@@ -0,0 +1,22 @@
+document.write('\
+\
+<div id="navigate">\
+<h2 class="hide">Menu:</h2>\
+<ul>\
+	<li><a href="index.html">Home</a></li>\
+	<li><a href="publications.html">Publications</a></li>\
+	<li><a href="collaborators.html">Collaborators</a></li>\
+	<li><a href="editorial.html">Editorial</a></li>\
+	<li><a href="workshops.html">Workshops</a></li>\
+	<li><a href="events.html">Events</a></li>\
+	<li><a href="publishing.html">Publishing</a></li>\
+	<li><a href="history.html">History</a></li>\
+	<li><a href="erdos.html">Erd&#337;s number</a></li>\
+	<li><a href="students.html">Students</a></li>\
+	<li><a href="teaching.html">Teaching</a></li>\
+	<li><a href="sport.html">Sport</a></li>\
+	<li><a href="contact.html">Contact</a></li>\
+</ul>\
+</div>\
+\
+');
