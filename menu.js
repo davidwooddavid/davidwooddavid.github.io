5d7db6d22e4f46d2eefd5c6570e3de6ad462230f
davidwooddavid <43978638+davidwooddavid@users.noreply.github.com>
2025-03-08 09:49:14 +1100

    add

--- menu.js
+++ menu.js
@@ -5,10 +5,10 @@ document.write('\
 <ul>\
 	<li><a href="index.html">Home</a></li>\
 	<li><a href="publications.html">Publications</a></li>\
+	<li><a href="news.html">News</a></li>\
 	<li><a href="collaborators.html">Collaborators</a></li>\
 	<li><a href="editorial.html">Editorial</a></li>\
 	<li><a href="workshops.html">Workshops</a></li>\
-	<li><a href="events.html">Events</a></li>\
 	<li><a href="publishing.html">Publishing</a></li>\
 	<li><a href="history.html">History</a></li>\
 	<li><a href="erdos.html">Erd&#337;s number</a></li>\
